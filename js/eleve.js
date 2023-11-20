e301188d32e835b5d0fbcc0a6de504145fd4d29e
QuentinG22 <quentin-guillemin@orange.fr>
2023-11-20 16:24:36 +0100

    ajout de méthode dans eleve.js

--- js/eleve.js
+++ js/eleve.js
@@ -9,4 +9,19 @@ class Eleve {
             JavaScript: []
         }
     }
+    bonjour() {
+        console.log(`Je suis ${this.prenom} ${this.nom} en classe ${this.classe}`)
+    }
+    ajouterNoteAlgorithmique(laNoteAlgo) {
+        this.notes.algorithmique.push(laNoteAlgo)
+        console.log(`Note d'algorithmique ajoutée: ${laNoteAlgo}`)
+    }
+    ajouterNoteJavaScript(laNote) {
+        this.notes.JavaScript.push(laNote)
+        console.log(`Note JavaScript ajoutée: ${laNote}`)
+    }
+    ajouterNote(matiere, tableau) {
+        this.notes[matiere] = tableau
+        console.log(`Notes de ${matiere} ajoutées: ${tableau}`)
+    }
 }
