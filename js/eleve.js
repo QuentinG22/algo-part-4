9f7df992db7ea6c52a4b2b053a0bf604f866d7ec
QuentinG22 <quentin-guillemin@orange.fr>
2023-11-21 15:24:15 +0100

    Commentaire ajouter

--- js/eleve.js
+++ js/eleve.js
@@ -12,17 +12,21 @@ class Eleve {
     bonjour() {
         console.log(`Je suis ${this.prenom} ${this.nom} en classe ${this.classe}`)
     }
+    // Pour ajouter une note à la matière Algorithmique
     ajouterNoteAlgorithmique(laNoteAlgo) {
         this.notes.algorithmique.push(laNoteAlgo)
     }
+    // Pour ajouter une note à la matière JavaScript
     ajouterNoteJavaScript(laNoteJavaScript) {
         this.notes.JavaScript.push(laNoteJavaScript)
     }
+    // Pour ajouter une note à une matière choisi grâce au paramètre
+    ajouterNote(matiere, laNote) {
+        this.notes[matiere].push(laNote)
+    }
+    // Ajouter une matière à this.notes
     ajouterMatiere(matiere) {
         this.notes[matiere] = []
         console.log(`La matière ${matiere} à été ajouter`)
-    }
-    ajouterNote(matiere, laNote) {
-        this.notes[matiere].push(laNote)
     }
 }
--- js/tools.js
+++ js/tools.js
@@ -6,3 +6,12 @@ function generateurNote() {
     return Math.floor(Math.random() * (max - min)) // retourne une valeur entier entre 0 et 20
 
 }
+
+function affichageTableau(obj) {
+    let affichage = 'Contenu des matières:\n\n';
+    for (let i in obj) {
+        affichage += "Matière: " + i + " -> notes: " + obj[i] + "\n";
+    }
+
+    console.log(affichage);
+}
